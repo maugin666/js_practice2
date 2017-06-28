0ac8059b11cc597e13f4dbfc4b8915690dfd9086
Eugenie Levanova <maugin666@gmail.com>
2017-06-28 21:50:53 +0500

    fix progect

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,24 @@
 var
   gulp = require('gulp'),
   sass = require('gulp-sass'),
-  haml = require('gulp-haml'),
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer');
 
-/*gulp.task('haml', function () {
-  gulp.src('public/*.haml')
-  .pipe(haml(haml({
-    compiler: 'visionmedia'
-  })))
-  .pipe(gulp.dest('public/'));
-});*/
-
 gulp.task('sass', function () {
-  return gulp.src('public/styles/scss/*.scss')
+  return gulp.src('dev/styles/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public/styles/css'));
+    .pipe(gulp.dest('dev/styles/css'));
 });
 
 gulp.task('autoprefixer', ['sass'], function () {
-  return gulp.src('public/styles/css/*.css')
+  return gulp.src('dev/styles/css/*.css')
     .pipe(postcss([ autoprefixer() ]))
-    .pipe(gulp.dest('public/styles/css/'));
+    .pipe(gulp.dest('dev/styles/css/'));
 });
 
 gulp.task('watch', ['sass', 'autoprefixer'], function() {
-  gulp.watch('public/styles/scss/*.scss', ['sass']);
-  gulp.watch('public/styles/css/*.css', ['autoprefixer']);
+  gulp.watch('dev/styles/scss/*.scss', ['sass']);
+  gulp.watch('dev/styles/css/*.css', ['autoprefixer']);
 });
 
 gulp.task('run', ['sass', 'autoprefixer']);
